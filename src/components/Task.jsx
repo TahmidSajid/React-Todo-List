bffa784e12cd098e938952164b3b230176d43cdc
TahmidSajid <tahmidkhansajid@gmail.com>
2026-04-22 21:08:32 +0600

    Task shown

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Task = ({ id, taskName, desc }) => {
   return (
     <>
-      <div className="card">
+      <div className="card mb-4">
         <div className="card-body">
           <h5 className="card-title">{taskName}</h5>
             <p>{desc}</p>
